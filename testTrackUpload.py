7c0ee494b03c849803a3bb282d72294f5d8474aa
Waldemar Smirnow <wsmirnow@uni-osnabrueck.de>
2020-10-21 03:43:33 +0200

    post multi part form data variant 2

--- testTrackUpload.py
+++ testTrackUpload.py
@@ -6,7 +6,7 @@ from httpx import DigestAuth, get, post
 import config
 
 
-def main()
+def main():
     auth = DigestAuth(config.targetuser, config.targetpassword)
     print('create mediapackage')
     resp = get(config.targetserver + "/ingest/createMediaPackage", headers=config.header, auth=auth)
@@ -15,8 +15,9 @@ def main()
     print(f'mediapackage: {mp}')
 
     print('uploading track.mp4')
-    payload = {'flavor': 'presentation/source', 'mediaPackage': mp, 'tags': 'archive, foo', 'BODY': ('track.mp4', open('track.mp4', 'rb'))}
-    resp = post(config.targetserver + "/ingest/addTrack", headers=config.header, auth=auth, files=payload)
+    data = {'flavor': 'presentation/source', 'mediaPackage': mp, 'tags': 'archive, foo'}
+    files = {'BODY': ('track.mp4', open('track.mp4', 'rb'), 'video/mp4')}
+    resp = post(config.targetserver + "/ingest/addTrack", headers=config.header, auth=auth, data=data, files=files)
     resp.raise_for_status()
     mp = resp.text
     print(f'mediapackage: {mp}')
